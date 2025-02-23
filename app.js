4a63c3d55e7bfec4dfd54a9e854d76b5169bcf1f
Alejandro Rey Vera <alejoreyv@gmail.com>
2025-02-22 22:52:20 -0500

    Realizo la terminacion de 2 tarjetas de JavaScript que las cuales son 'Crear un array para almacenar los nombres' y la otra que es 'Implmenta una funcion para agregar amigos' Esos fueron los avances.

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+
+// Creo un Array para almacenar los nombre.
 let amigos = [];
 
-console.log(amigos.length);
+console.log(amigos);
+
+
+/**
+ * 
+ * Implemento la funcion para agregar amigos.
+ *  
+ */
 
 function agregarAmigo () {
+    let IngresaNombreAmigo = document.getElementById('amigo');
+    let nombreDeAmigo = IngresaNombreAmigo.value.trim();
 
+    // Luego de Haber ingresado, Valido la entrada para que el campo no este vacio.
+
+    if (nombreDeAmigo === '') {
+        alert('Por favor, inserte un nombre para agregar en amigo secreto')
+        return;
+    }
+
+    // Luego de validar con la alerta. Se actualiza el Array de amigos, para mirar si es valido, añadirlo al arrglo.
+    amigos.push(nombreDeAmigo)
+
+    
+    // Luego de haber validado, Se hace el proceso de LIMPIAR EL CAMPO DE ENTRADA, despues de añadir el nombre.
+    IngresaNombreAmigo.value = '';
 }
+
+/**
+ * 
+ * Implementacion de una Funcion para actualizar la lista de amigos
+ * 
+ */
+
+//
+
+
