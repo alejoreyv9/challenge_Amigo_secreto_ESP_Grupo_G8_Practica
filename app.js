70178613b28191cd37c1aa3df703577a3915b2e4
Alejandro Rey Vera <alejoreyv@gmail.com>
2025-02-24 23:32:38 -0500

    Termino todas las tarjetas de JavaScript, se crea la funcionalidad del Amigo secreto del Challenge cumple con las espectvas propuestas. Queda pendiente avanzar en el punto en Realizar el README

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ console.log(amigos);
  *  
  */
 
-function agregarAmigo () {
+function agregarAmigo() {
     let IngresaNombreAmigo = document.getElementById('amigo');
     let nombreDeAmigo = IngresaNombreAmigo.value.trim();
 
@@ -27,17 +27,69 @@ function agregarAmigo () {
     // Luego de validar con la alerta. Se actualiza el Array de amigos, para mirar si es valido, añadirlo al arrglo.
     amigos.push(nombreDeAmigo)
 
-    
+
     // Luego de haber validado, Se hace el proceso de LIMPIAR EL CAMPO DE ENTRADA, despues de añadir el nombre.
     IngresaNombreAmigo.value = '';
+
+/**
+* 
+* Implementacion de una Funcion para actualizar la lista de amigos
+* 
+*/
+    // Obtener el elemento de la lista
+    let listaAmigosElement = document.getElementById('listaAmigos')
+
+    // Limpiar la lista existente.
+    listaAmigosElement.innerHTML = "";
+
+    // Iterar sobre el arreglo.
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i];
+
+        // Creacion del Elemento en Formato <li>
+        let elementLi = document.createElement('li');
+        elementLi.textContent = amigo; // Aqui se asignara el nombre del amigo en texto del <li>
+
+        // Agregar elementos a la lista
+        listaAmigosElement.appendChild(elementLi);
+    }
+
 }
 
 /**
  * 
- * Implementacion de una Funcion para actualizar la lista de amigos
+ * Implmenta una funcion para sortear los amigos
  * 
  */
 
-//
+function sortearAmigo() {
+    // Validar que haya amigos Disponibles.
+    if (amigos.length === 0) {
+        alert('Ingresa amigos para poder sortearlo.')
+        return;
+    }
+
+    // Generar un indice aletorio.
+    let aletorioIndice = Math.floor(Math.random() * amigos.length);
+
+    // Obtener el nombre sorteado.
+    let sorteoAmigos = amigos[aletorioIndice];
+
+    // Mostrar los resultamos.
+    let resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = `<li> El Amigo sortiado es: ${sorteoAmigos}</li>`;
+
+}
 
 
+/**
+ * 
+ * -------NOTA-------
+ * 
+ * Gracias por la oportunidad. Quiero decir que espero una retroalimentación
+ * para asi mejorar y poder aprender mucho mas espero poder cogerle fuerte al JavaScript y  algunos lenguajes de mas.
+ * Cabe decir que el codigo tiene orden acorde lo pedido de cada tarjeta paso a paso.
+ * 
+ * Gracias por sus atención prestada.
+ */
